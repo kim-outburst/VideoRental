fec416991de2045776acca1288a34a5fc4bef83b
Kyungtae Kim <1123kt@gmail.com>
2022-11-23 11:30:02 +0900

    extract dayrened

--- src/VideoRental/model/Rental.java
+++ src/VideoRental/model/Rental.java
@@ -54,15 +54,8 @@ public class Rental {
 
 	public int getDaysRentedLimit() {
 		int limit = 0 ;
-		int daysRented ;
-		if (getStatus() == 1) { // returned Video
-			long diff = returnDate.getTime() - rentDate.getTime();
-			daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
-		} else { // not yet returned
-			long diff = new Date().getTime() - rentDate.getTime();
-			daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
-		}
-		if ( daysRented <= 2) return limit ;
+
+		if ( getDaysRented() <= 2) return limit ;
 
 		switch ( video.getVideoType() ) {
 			case Video.VHS: limit = 5 ; break ;
@@ -70,5 +63,16 @@ public class Rental {
 			case Video.DVD: limit = 2 ; break ;
 		}
 		return limit ;
+	}
+
+	private int getDaysRented() {
+		int daysRented;
+		if (getStatus() == 1) { // returned Video
+			long diff = returnDate.getTime() - rentDate.getTime();
+		} else { // not yet returned
+			long diff = new Date().getTime() - rentDate.getTime();
+		}
+		daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
+		return daysRented;
 	}
 }
